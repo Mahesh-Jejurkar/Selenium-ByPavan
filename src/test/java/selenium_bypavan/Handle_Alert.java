4112465aef3373a90929f64296aaa54acf33331f
MaheshJejurkar <jejurkar.mahesh@gmail.com>
2024-08-27 14:53:13 +0530

    Code change.

---
+++ src/test/java/selenium_bypavan/Handle_Alert.java
@@ -0,0 +1,8 @@
+package selenium_bypavan;
+
+public class Handle_Alert {
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/test/java/selenium_bypavan/Handle_Checkbox.java
+++ src/test/java/selenium_bypavan/Handle_Checkbox.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class HandleCheckboxes extends BaseClass {
+public class Handle_Checkbox extends BaseClass {
 
 	public static void main(String[] args) throws Exception {
 		WebDriver driver = getDriver();

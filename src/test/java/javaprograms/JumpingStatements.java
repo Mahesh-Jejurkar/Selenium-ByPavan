19f833884c0ea59ad99664a87bbdd9f500e0d874
MaheshJejurkar <jejurkar.mahesh@gmail.com>
2024-09-02 17:02:27 +0530

    dss

---
+++ src/test/java/javaprograms/JumpingStatements.java
@@ -0,0 +1,37 @@
+package javaprograms;
+
+public class JumpingStatements {
+
+	public static void main(String[] args) {
+		for(int i=0; i<=10; i++) {
+			if(i==5) {
+				break;
+			}
+			System.out.println(i);
+		}
+		
+		System.out.println("....................");
+		
+		for(int i=0; i<=10; i++) {
+			if(i==3|| i==5 || i==7) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		System.out.println("....................");
+		for(int i=0; i<=10; i++) {
+			if(i%2==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		System.out.println("....................");
+		for(int i=0; i<=10; i++) {
+			if(!(i%2==0)) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}
--- src/test/java/javaprograms/VariablesDemo.java
+++ src/test/java/javaprograms/VariablesDemo.java
@@ -1,5 +1,7 @@
 package javaprograms;
 
+import io.netty.util.internal.SystemPropertyUtil;
+
 public class VariablesDemo {
 
 	public static void main(String[] args) {
@@ -11,9 +13,37 @@ public class VariablesDemo {
 		byte a = -128;
 		System.out.println(a);
 		
-		boolean x=false;
+		boolean x=true;
 		boolean y=false;
 		
 		System.out.println(x||y);
+		System.out.println(x&&y);
+		
+		boolean b1=10<20;
+		System.out.println(b1);
+		
+		
+		int b=10;
+		b++;
+		System.out.println(b);
+		
+		
+		int c=10;
+		int res2=++c;
+		System.out.println(res2);
+		System.out.println(++c);
+		
+		int d=200;
+		int e=100;
+		
+		int res=(d>e)?d:e;
+			
+		System.out.println(res);
+		
+		int age=10;
+		
+		String vote=(age>18)?"Eligible":"Not Eligible";
+		System.out.println(vote);
+		
 	}
 }

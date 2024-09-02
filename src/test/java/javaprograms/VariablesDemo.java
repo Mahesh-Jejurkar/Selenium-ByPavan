210e44f951ec438e02de3d858fb796e6e3df8148
MaheshJejurkar <jejurkar.mahesh@gmail.com>
2024-09-02 10:51:24 +0530

    New code added.

---
+++ src/test/java/javaprograms/VariablesDemo.java
@@ -0,0 +1,19 @@
+package javaprograms;
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		char grade = 'A';
+		String grade2 = "A";
+		System.out.println(grade);
+		System.out.println(grade2);
+		
+		byte a = -128;
+		System.out.println(a);
+		
+		boolean x=false;
+		boolean y=false;
+		
+		System.out.println(x||y);
+	}
+}

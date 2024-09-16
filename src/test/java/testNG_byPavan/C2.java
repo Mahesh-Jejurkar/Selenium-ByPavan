b9f1c833e918b572f1d15a43ef0f605bbcdf0319
MaheshJejurkar <jejurkar.mahesh@gmail.com>
2024-09-16 11:54:20 +0530

    Added new files.

---
+++ src/test/java/testNG_byPavan/C2.java
@@ -0,0 +1,12 @@
+package testNG_byPavan;
+
+import org.testng.annotations.*;
+
+public class C2 {
+	
+	@Test
+	void xyz() {
+		System.out.println("\tThis is xyz() from C2");
+	}
+
+}
---
+++ src/test/java/testNG_byPavan/C3.java
@@ -0,0 +1,22 @@
+package testNG_byPavan;
+
+import org.testng.annotations.*;
+
+public class C3 {
+	
+	@BeforeSuite
+	void bs() {
+		System.out.println("This is BeforeSuite method.");
+	}
+	
+	@AfterSuite
+	void as() {
+		System.out.println("This is AfterSuite method.");
+	}
+	
+	@Test
+	void pqr() {
+		System.out.println("\tThis is pqr() from C3");
+	}
+
+}

e5bc1cfd8dffcbee57562ae31b282f4c271974d6
MaheshJejurkar <jejurkar.mahesh@gmail.com>
2024-09-18 08:43:50 +0530

    New code added.

--- src/test/java/javaprograms/DateDemo.java
+++ src/test/java/javaprograms/DateDemo.java
@@ -9,9 +9,15 @@ public class DateDemo {
 		Date objDate = new Date();
 		System.out.println(objDate);
 
+		System.out.println();
 		String StringDateFormat = "yyyy.MM.dd EEEE HH:mm:ss a z";
 		SimpleDateFormat SmplDateFormat = new SimpleDateFormat(StringDateFormat);
 		System.out.println(SmplDateFormat.format(objDate));
+		
+		
+		System.out.println();
+		String timestamp = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss-a").format(new Date());
+		System.out.println(timestamp);
 	}
 
 }
